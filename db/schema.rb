2eb436aead0f2568bdaafb0a66401e8306bcfec6
yamamoki <112685611+yamamoki@users.noreply.github.com>
2022-12-06 01:12:48 +0000

    models

--- db/schema.rb
+++ db/schema.rb
@@ -57,6 +57,8 @@ ActiveRecord::Schema.define(version: 2022_12_05_141848) do
   end
 
   create_table "relationships", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "followed_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
