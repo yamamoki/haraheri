0eae005dbd4c9f8207c1d529730abddbfe6ca28c
yamamoki <112685611+yamamoki@users.noreply.github.com>
2022-12-12 07:53:38 +0000

    edit

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -5,9 +5,22 @@ class Public::UsersController < ApplicationController
   end
 
   def edit
-    
+    @user = current_user
   end
 
   def check
+  end
+
+  def update
+    @user = current_user
+    @user.update(user_params)
+    redirect_to users_path(current_user)
+  end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email)
   end
 end
