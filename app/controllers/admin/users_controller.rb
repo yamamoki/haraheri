9171faf583a7f1a78ba7911fca4826e20f376a18
yamamoki <112685611+yamamoki@users.noreply.github.com>
2022-12-14 13:29:42 +0000

    [add]admin

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,11 +1,27 @@
 class Admin::UsersController < ApplicationController
+  before_action :authenticate_admin!
+
   def index
     @users = User.all
+    @user = User.page(params[:page]).per(10)
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def edit
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to admin_users_path(@user)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :is_deleted)
   end
 end
