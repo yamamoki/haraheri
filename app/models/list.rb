c1610bb604b2af31a5ac0d6ca4ab0b4847b997ce
yamamoki <112685611+yamamoki@users.noreply.github.com>
2022-12-15 15:44:54 +0000

    [add]tag

--- app/models/list.rb
+++ app/models/list.rb
@@ -2,7 +2,7 @@ class List < ApplicationRecord
  has_one_attached :image
  has_many :post_comments, dependent: :destroy
  has_many :favorites, dependent: :destroy
- 
+
  has_many :post_tags, dependent: :destroy
  has_many :tags, through: :post_tags, dependent: :destroy
 
