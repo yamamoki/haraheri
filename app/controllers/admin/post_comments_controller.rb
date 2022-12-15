9e028aacf77d7b0293b8a249d4892ef2d736c624
yamamoki <112685611+yamamoki@users.noreply.github.com>
2022-12-15 03:32:44 +0000

    [add]admin_comment

--- app/controllers/admin/post_comments_controller.rb
+++ app/controllers/admin/post_comments_controller.rb
@@ -1,2 +1,6 @@
 class Admin::PostCommentsController < ApplicationController
+  def destroy
+    @post_comment = PostComment.find(params[:id]).destroy
+    redirect_to admin_list_path(params[:list_id])
+  end
 end
