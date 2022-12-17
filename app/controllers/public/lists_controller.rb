0879076d784dbb689c5e8ba0c67baa7f42fc61f5
yamamoki <112685611+yamamoki@users.noreply.github.com>
2022-12-17 07:10:37 +0000

    削除機能

--- app/controllers/public/lists_controller.rb
+++ app/controllers/public/lists_controller.rb
@@ -43,7 +43,7 @@ class Public::ListsController < ApplicationController
 
   def destroy
     @list = List.find(params[:id]).destroy
-    redirect_to list_path(list)
+    redirect_to lists_path(@list)
   end
 
 
