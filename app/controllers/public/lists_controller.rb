9f109ff6ec0560d3fe583bed8acfcf6b10e6a078
yamamoki <112685611+yamamoki@users.noreply.github.com>
2022-12-19 05:30:11 +0000

    表示順

--- app/controllers/public/lists_controller.rb
+++ app/controllers/public/lists_controller.rb
@@ -1,13 +1,13 @@
 class Public::ListsController < ApplicationController
   def index
     if params[:search].blank? && params[:tag_id].blank?
-      @lists= List.where(draft: 'release').page(params[:page]).per(6)
+      @lists= List.where(draft: 'release').page(params[:page]).order('id DESC').per(6)
     elsif params[:search].present? && params[:tag_id].blank?
-      @lists= List.where("title LIKE ? or material LIKE ? or body LIKE ? ",'%' + params[:search] + '%','%' + params[:search] + '%','%' + params[:search] + '%').page(params[:page]).per(6)
+      @lists= List.where("title LIKE ? or material LIKE ? or body LIKE ? ",'%' + params[:search] + '%','%' + params[:search] + '%','%' + params[:search] + '%').page(params[:page]).order('id DESC').per(6)
     elsif params[:search].blank? && params[:tag_id].present?
-      @lists = Tag.find(params[:tag_id]).lists.page(params[:page]).per(6)
+      @lists = Tag.find(params[:tag_id]).lists.page(params[:page]).order('id DESC').per(6)
     else
-      @lists = Tag.find(params[:tag_id]).lists.where("title LIKE ? or material LIKE ? or body LIKE ? ",'%' + params[:search] + '%','%' + params[:search] + '%','%' + params[:search] + '%').page(params[:page]).per(6)
+      @lists = Tag.find(params[:tag_id]).lists.where("title LIKE ? or material LIKE ? or body LIKE ? ",'%' + params[:search] + '%','%' + params[:search] + '%','%' + params[:search] + '%').page(params[:page]).order('id DESC').per(6)
     end
   end
 
