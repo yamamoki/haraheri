854432a501d8ade6b9a01906bb473d1950d05d52
yamamoki <112685611+yamamoki@users.noreply.github.com>
2022-12-19 17:53:25 +0000

    不要ファイル削除

--- app/controllers/admin/post_comments_controller.rb
+++ app/controllers/admin/post_comments_controller.rb
@@ -1,6 +1,7 @@
 class Admin::PostCommentsController < ApplicationController
   def destroy
     @post_comment = PostComment.find(params[:id]).destroy
+    flash[:alert] = "削除しました"
     redirect_to admin_list_path(params[:list_id])
   end
 end
