4509b544b988fb1cfe66540d22f7a13212cb6506
yamamoki <112685611+yamamoki@users.noreply.github.com>
2022-12-20 16:55:01 +0000

    テストデータ

--- db/migrate/20221205141202_create_lists.rb
+++ db/migrate/20221205141202_create_lists.rb
@@ -2,7 +2,6 @@ class CreateLists < ActiveRecord::Migration[6.1]
   def change
     create_table :lists do |t|
       t.integer :user_id
-      t.integer :genre_id
       t.string :title
       t.text :material
       t.text :body
