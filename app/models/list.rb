08d79bde76f8e7e14a86d685b1317e9b555986e7
yamamoki <112685611+yamamoki@users.noreply.github.com>
2023-01-20 02:20:03 +0000

    test2

--- app/models/list.rb
+++ app/models/list.rb
@@ -2,7 +2,6 @@ class List < ApplicationRecord
  has_one_attached :image
  has_many :post_comments, -> { order(created_at: :desc) }, dependent: :destroy
  has_many :favorites, dependent: :destroy
-
  has_many :post_tags, dependent: :destroy
  has_many :tags, through: :post_tags, dependent: :destroy
 
